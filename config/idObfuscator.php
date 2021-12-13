5bd56571596a71d48652dde2a8494b59be035d8a
brecht.vermeersch <brecht.vermeersch@athena-graphics.be>
2021-12-13 13:28:50 +0100

    Fix driver config

--- config/idObfuscator.php
+++ config/idObfuscator.php
@@ -17,37 +17,21 @@ return [
 
     'default' => env('ID_OBFUSCATOR_DRIVER', 'null'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | IdObfuscator Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define all of the obfuscator connections that will be used
-    | to encode or decode ids. Samples of each available type of connection are \
-    | provided inside this array.
-    |
-    */
-
-    'connections' => [
+    'drivers' => [
 
         'hashids' => [
-            'driver' => 'hashids',
             'salt' => env('HASHIDS_SALT'),
             'minimumHashLength' => env('HASHIDS_MINIMUM_HASH_LENGTH'),
             'alphabet' => env('HASHIDS_ALPHABET')
         ],
 
         'optimus' => [
-            'driver' => 'optimus',
             'prime' => env('OPTIMUS_PRIME'),
             'inverse' => env('OPTIMUS_INVERSE'),
             'random' => env('OPTIMUS_RANDOM')
         ],
 
-        'null' => [
-            'driver' => 'null',
-        ],
-
+        'null' => [],
     ],
 
 ];
